fdf142750f93aa37ade28a12d548840554674473
Condition17 <c.conache@gmail.com>
2020-10-20 00:07:37 +0300

    Fixed run controller service

--- run-controller-service/main.go
+++ run-controller-service/main.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	"run-controller-service/handler"
-	"run-controller-service/subscriber"
-
 	"github.com/micro/go-micro/v2"
 	log "github.com/micro/go-micro/v2/logger"
-
-	runcontrollerservice "run-controller-service/proto/run-controller-service"
 )
 
 func main() {
@@ -19,12 +14,6 @@ func main() {
 
 	// Initialise service
 	service.Init()
-
-	// Register Handler
-	runcontrollerservice.RegisterRunControllerServiceHandler(service.Server(), new(handler.RunControllerService))
-
-	// Register Struct as Subscriber
-	micro.RegisterSubscriber("go.micro.service.run-controller-service", service.Server(), new(subscriber.RunControllerService))
 
 	// Run service
 	if err := service.Run(); err != nil {
