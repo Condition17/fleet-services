154681af0b4d5f81f96a8a2fba610f7e8c7a5e04
Condition17 <c.conache@gmail.com>
2020-10-23 22:16:11 +0300

    Moved base handler code to common dir

--- common/go.mod
+++ common/go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/Condition17/fleet-services/user-service v0.0.0-20201018140155-bea1c2e00e55
+	github.com/micro/go-micro v1.18.0
 	github.com/micro/go-micro/v2 v2.9.1
 	gorm.io/gorm v1.20.2
 )
---
+++ common/handler/base-handler.go
@@ -0,0 +1,33 @@
+package handler
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/Condition17/fleet-services/common/auth"
+	"github.com/micro/go-micro/v2"
+	"github.com/micro/go-micro/v2/broker"
+)
+
+const runStateTopic = "test-run-state"
+
+type BaseHandler struct {
+	Service        micro.Service
+	MessagesBroker broker.Broker
+}
+
+func NewBaseHandler(service micro.Service) BaseHandler {
+	return BaseHandler{Service: service, MessagesBroker: service.Server().Options().Broker}
+}
+
+func (h *BaseHandler) sendRunStateEvent(ctx context.Context, eventType string, data string) {
+	var usrDetails string = string(auth.GetUserBytesFromContext(ctx))
+	msgBody, _ := json.Marshal(&runControllerProto.Event{Type: eventType, User: usrDetails, Data: data})
+	fmt.Printf("Sending run state event on topic %s - Body: %v\n", runStateTopic, msgBody)
+
+	if err := h.MessagesBroker.Publish(runStateTopic, &broker.Message{Body: msgBody}); err != nil {
+		log.Printf("[Messages Broker] Failed to publish message on create. Encountered error: %v", err)
+	}
+}
