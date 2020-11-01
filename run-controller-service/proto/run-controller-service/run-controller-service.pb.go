5400ac9e44b5927b7f50137f2a13cba1a3dc05b1
Condition17 <c.conache@gmail.com>
2020-11-01 15:16:07 +0200

    Updated run-controller proto

--- run-controller-service/proto/run-controller-service/run-controller-service.pb.go
+++ run-controller-service/proto/run-controller-service/run-controller-service.pb.go
@@ -316,6 +316,171 @@ func (x *TestRunCreatedEventData) GetFileSpec() *FileSpec {
 	return nil
 }
 
+type WssEvent struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Type string `protobuf:"bytes,1,opt,name=type,proto3" json:"type,omitempty"`
+	Data []byte `protobuf:"bytes,2,opt,name=data,proto3" json:"data,omitempty"`
+}
+
+func (x *WssEvent) Reset() {
+	*x = WssEvent{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_proto_run_controller_service_run_controller_service_proto_msgTypes[5]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *WssEvent) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*WssEvent) ProtoMessage() {}
+
+func (x *WssEvent) ProtoReflect() protoreflect.Message {
+	mi := &file_proto_run_controller_service_run_controller_service_proto_msgTypes[5]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use WssEvent.ProtoReflect.Descriptor instead.
+func (*WssEvent) Descriptor() ([]byte, []int) {
+	return file_proto_run_controller_service_run_controller_service_proto_rawDescGZIP(), []int{5}
+}
+
+func (x *WssEvent) GetType() string {
+	if x != nil {
+		return x.Type
+	}
+	return ""
+}
+
+func (x *WssEvent) GetData() []byte {
+	if x != nil {
+		return x.Data
+	}
+	return nil
+}
+
+type WssErrorMessageData struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Type    string `protobuf:"bytes,1,opt,name=type,proto3" json:"type,omitempty"`
+	Message string `protobuf:"bytes,2,opt,name=message,proto3" json:"message,omitempty"`
+}
+
+func (x *WssErrorMessageData) Reset() {
+	*x = WssErrorMessageData{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_proto_run_controller_service_run_controller_service_proto_msgTypes[6]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *WssErrorMessageData) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*WssErrorMessageData) ProtoMessage() {}
+
+func (x *WssErrorMessageData) ProtoReflect() protoreflect.Message {
+	mi := &file_proto_run_controller_service_run_controller_service_proto_msgTypes[6]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use WssErrorMessageData.ProtoReflect.Descriptor instead.
+func (*WssErrorMessageData) Descriptor() ([]byte, []int) {
+	return file_proto_run_controller_service_run_controller_service_proto_rawDescGZIP(), []int{6}
+}
+
+func (x *WssErrorMessageData) GetType() string {
+	if x != nil {
+		return x.Type
+	}
+	return ""
+}
+
+func (x *WssErrorMessageData) GetMessage() string {
+	if x != nil {
+		return x.Message
+	}
+	return ""
+}
+
+type FileEntityCreatedEventData struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	TestRunId uint32 `protobuf:"varint,1,opt,name=testRunId,proto3" json:"testRunId,omitempty"`
+	FileId    string `protobuf:"bytes,2,opt,name=fileId,proto3" json:"fileId,omitempty"`
+}
+
+func (x *FileEntityCreatedEventData) Reset() {
+	*x = FileEntityCreatedEventData{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_proto_run_controller_service_run_controller_service_proto_msgTypes[7]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *FileEntityCreatedEventData) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*FileEntityCreatedEventData) ProtoMessage() {}
+
+func (x *FileEntityCreatedEventData) ProtoReflect() protoreflect.Message {
+	mi := &file_proto_run_controller_service_run_controller_service_proto_msgTypes[7]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use FileEntityCreatedEventData.ProtoReflect.Descriptor instead.
+func (*FileEntityCreatedEventData) Descriptor() ([]byte, []int) {
+	return file_proto_run_controller_service_run_controller_service_proto_rawDescGZIP(), []int{7}
+}
+
+func (x *FileEntityCreatedEventData) GetTestRunId() uint32 {
+	if x != nil {
+		return x.TestRunId
+	}
+	return 0
+}
+
+func (x *FileEntityCreatedEventData) GetFileId() string {
+	if x != nil {
+		return x.FileId
+	}
+	return ""
+}
+
 var File_proto_run_controller_service_run_controller_service_proto protoreflect.FileDescriptor
 
 var file_proto_run_controller_service_run_controller_service_proto_rawDesc = []byte{
@@ -357,9 +522,21 @@ var file_proto_run_controller_service_run_controller_service_proto_rawDesc = []b
 	0x72, 0x6f, 0x2e, 0x73, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x2e, 0x72, 0x75, 0x6e, 0x63, 0x6f,
 	0x6e, 0x74, 0x72, 0x6f, 0x6c, 0x6c, 0x65, 0x72, 0x73, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x2e,
 	0x46, 0x69, 0x6c, 0x65, 0x53, 0x70, 0x65, 0x63, 0x52, 0x08, 0x66, 0x69, 0x6c, 0x65, 0x53, 0x70,
-	0x65, 0x63, 0x32, 0x16, 0x0a, 0x14, 0x52, 0x75, 0x6e, 0x43, 0x6f, 0x6e, 0x74, 0x72, 0x6f, 0x6c,
-	0x6c, 0x65, 0x72, 0x53, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74,
-	0x6f, 0x33,
+	0x65, 0x63, 0x22, 0x32, 0x0a, 0x08, 0x57, 0x73, 0x73, 0x45, 0x76, 0x65, 0x6e, 0x74, 0x12, 0x12,
+	0x0a, 0x04, 0x74, 0x79, 0x70, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x74, 0x79,
+	0x70, 0x65, 0x12, 0x12, 0x0a, 0x04, 0x64, 0x61, 0x74, 0x61, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0c,
+	0x52, 0x04, 0x64, 0x61, 0x74, 0x61, 0x22, 0x43, 0x0a, 0x13, 0x57, 0x73, 0x73, 0x45, 0x72, 0x72,
+	0x6f, 0x72, 0x4d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x44, 0x61, 0x74, 0x61, 0x12, 0x12, 0x0a,
+	0x04, 0x74, 0x79, 0x70, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x74, 0x79, 0x70,
+	0x65, 0x12, 0x18, 0x0a, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x18, 0x02, 0x20, 0x01,
+	0x28, 0x09, 0x52, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x22, 0x52, 0x0a, 0x1a, 0x46,
+	0x69, 0x6c, 0x65, 0x45, 0x6e, 0x74, 0x69, 0x74, 0x79, 0x43, 0x72, 0x65, 0x61, 0x74, 0x65, 0x64,
+	0x45, 0x76, 0x65, 0x6e, 0x74, 0x44, 0x61, 0x74, 0x61, 0x12, 0x1c, 0x0a, 0x09, 0x74, 0x65, 0x73,
+	0x74, 0x52, 0x75, 0x6e, 0x49, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0d, 0x52, 0x09, 0x74, 0x65,
+	0x73, 0x74, 0x52, 0x75, 0x6e, 0x49, 0x64, 0x12, 0x16, 0x0a, 0x06, 0x66, 0x69, 0x6c, 0x65, 0x49,
+	0x64, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x06, 0x66, 0x69, 0x6c, 0x65, 0x49, 0x64, 0x32,
+	0x16, 0x0a, 0x14, 0x52, 0x75, 0x6e, 0x43, 0x6f, 0x6e, 0x74, 0x72, 0x6f, 0x6c, 0x6c, 0x65, 0x72,
+	0x53, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -374,13 +551,16 @@ func file_proto_run_controller_service_run_controller_service_proto_rawDescGZIP(
 	return file_proto_run_controller_service_run_controller_service_proto_rawDescData
 }
 
-var file_proto_run_controller_service_run_controller_service_proto_msgTypes = make([]protoimpl.MessageInfo, 5)
+var file_proto_run_controller_service_run_controller_service_proto_msgTypes = make([]protoimpl.MessageInfo, 8)
 var file_proto_run_controller_service_run_controller_service_proto_goTypes = []interface{}{
-	(*EventMetadata)(nil),           // 0: go.micro.service.runcontrollerservice.EventMetadata
-	(*Event)(nil),                   // 1: go.micro.service.runcontrollerservice.Event
-	(*TestRunSpec)(nil),             // 2: go.micro.service.runcontrollerservice.TestRunSpec
-	(*FileSpec)(nil),                // 3: go.micro.service.runcontrollerservice.FileSpec
-	(*TestRunCreatedEventData)(nil), // 4: go.micro.service.runcontrollerservice.TestRunCreatedEventData
+	(*EventMetadata)(nil),              // 0: go.micro.service.runcontrollerservice.EventMetadata
+	(*Event)(nil),                      // 1: go.micro.service.runcontrollerservice.Event
+	(*TestRunSpec)(nil),                // 2: go.micro.service.runcontrollerservice.TestRunSpec
+	(*FileSpec)(nil),                   // 3: go.micro.service.runcontrollerservice.FileSpec
+	(*TestRunCreatedEventData)(nil),    // 4: go.micro.service.runcontrollerservice.TestRunCreatedEventData
+	(*WssEvent)(nil),                   // 5: go.micro.service.runcontrollerservice.WssEvent
+	(*WssErrorMessageData)(nil),        // 6: go.micro.service.runcontrollerservice.WssErrorMessageData
+	(*FileEntityCreatedEventData)(nil), // 7: go.micro.service.runcontrollerservice.FileEntityCreatedEventData
 }
 var file_proto_run_controller_service_run_controller_service_proto_depIdxs = []int32{
 	0, // 0: go.micro.service.runcontrollerservice.Event.meta:type_name -> go.micro.service.runcontrollerservice.EventMetadata
@@ -459,6 +639,42 @@ func file_proto_run_controller_service_run_controller_service_proto_init() {
 				return nil
 			}
 		}
+		file_proto_run_controller_service_run_controller_service_proto_msgTypes[5].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*WssEvent); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_proto_run_controller_service_run_controller_service_proto_msgTypes[6].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*WssErrorMessageData); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_proto_run_controller_service_run_controller_service_proto_msgTypes[7].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*FileEntityCreatedEventData); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -466,7 +682,7 @@ func file_proto_run_controller_service_run_controller_service_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_proto_run_controller_service_run_controller_service_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   5,
+			NumMessages:   8,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
