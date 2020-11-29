0e3c7dc642e87f4a2693c0e701e73283ff11043f
Condition17 <c.conache@gmail.com>
2020-11-29 12:33:50 +0200

    Update circleci config

--- infrastructure/servers/file-builder/startup.sh
+++ infrastructure/servers/file-builder/startup.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
-# TODO: ensure port forwarding is setup
 WORKDIR=~/deploy
 mkdir -p $WORKDIR && cd $WORKDIR || exit
 
 # Download and extract service archive
-gsutil cp gs://fleet-deploy-binaries/file-builder.tar.gz
-tar -xzf file-builder.tar.gz .
+gsutil cp gs://fleet-deploy/binaries/file-builder.tar.gz file-builder.tar.gz
+tar -xzf file-builder.tar.gz
+
+# Download environment metadata
+mkdir env
+gsutil cp gs://fleet-deploy/env/file-builder env
 
 # Run server
-chmod +x ./server && ./server
+chmod +x ./server && \
+export ENV_NAME=prod && \
+./server
