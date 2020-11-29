390bb878db252f3fbc083c04604596ca4814ba9b
Condition17 <c.conache@gmail.com>
2020-11-29 17:12:52 +0200

    try use previous file service name

--- lib/utils.go
+++ lib/utils.go
@@ -9,7 +9,7 @@ import (
 
 func GetFullExternalServiceName(baseServiceName string) string {
 	if os.Getenv("ENV_NAME") == string(environment.ProdEnv) {
-		return fmt.Sprintf("%s:8080", baseServiceName)
+		return fmt.Sprintf("go.micro.api.%s:8080", baseServiceName)
 	}
 
 	return fmt.Sprintf("go.micro.api.%s", baseServiceName)
