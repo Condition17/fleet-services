6d307be30861da28a7f1ba4ce4016bdcaeb007e3
Condition17 <c.conache@gmail.com>
2020-12-07 00:21:12 +0200

    Add run states for executor instances provisioning

--- test-run-service/run-states/states.go
+++ test-run-service/run-states/states.go
@@ -3,29 +3,33 @@ package runStates
 type TestRunStateType string
 
 type runStatesEnumStruct struct {
-	Initiated        TestRunStateType
-	FileUpload       TestRunStateType
-	FileUploadDone   TestRunStateType
-	ProvisionFs      TestRunStateType
-	ProvisionFsDone  TestRunStateType
-	AssembleFile     TestRunStateType
-	AssembleFileDone TestRunStateType
-	Evaluating       TestRunStateType
-	Failed           TestRunStateType
-	Succeeded        TestRunStateType
-	Error            TestRunStateType
+	Initiated                     TestRunStateType
+	FileUpload                    TestRunStateType
+	FileUploadDone                TestRunStateType
+	ProvisionFs                   TestRunStateType
+	ProvisionFsDone               TestRunStateType
+	ProvisionExecutorInstance     TestRunStateType
+	ProvisionExecutorInstanceDone TestRunStateType
+	AssembleFile                  TestRunStateType
+	AssembleFileDone              TestRunStateType
+	Evaluating                    TestRunStateType
+	Failed                        TestRunStateType
+	Succeeded                     TestRunStateType
+	Error                         TestRunStateType
 }
 
 var TestRunState = runStatesEnumStruct{
-	Initiated:        "initiated",
-	FileUpload:       "file_upload",
-	FileUploadDone:   "file_upload_done",
-	ProvisionFs:      "provision_fs",
-	ProvisionFsDone:  "provision_fs_done",
-	AssembleFile:     "assemble_file",
-	AssembleFileDone: "assemble_file_done",
-	Evaluating:       "evaluating",
-	Failed:           "failed",
-	Succeeded:        "succeeded",
-	Error:            "error",
+	Initiated:                     "initiated",
+	FileUpload:                    "file_upload",
+	FileUploadDone:                "file_upload_done",
+	ProvisionFs:                   "provision_fs",
+	ProvisionFsDone:               "provision_fs_done",
+	ProvisionExecutorInstance:     "provision_executor_instance",
+	ProvisionExecutorInstanceDone: "provision_executor_instance_done",
+	AssembleFile:                  "assemble_file",
+	AssembleFileDone:              "assemble_file_done",
+	Evaluating:                    "evaluating",
+	Failed:                        "failed",
+	Succeeded:                     "succeeded",
+	Error:                         "error",
 }
